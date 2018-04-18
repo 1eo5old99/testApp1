9f3740f9db531c4aa468ba34699997d4f46f5261
Tanya Kozlova <1eo5old99@gmail.com>
2018-04-19 00:28:43 +0300

    add getDif

--- src/testapp1/TestApp1.java
+++ src/testapp1/TestApp1.java
@@ -7,10 +7,11 @@ public class TestApp1 {
 float a=10;
 float b=(float) 12.5;
 System.out.println("a+b="+(a+b));
- 
+ System.out.println("a-b="+(a-b));
   
 System.out.println("GIT test application");
         // TODO code application logic here
 System.out.println("GIT test application");
 }
+   
 }
