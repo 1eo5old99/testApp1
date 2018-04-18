bcb28ea1bcdcfc72736489f83e9fcc6ecba06e7a
Tanya Kozlova <1eo5old99@gmail.com>
2018-04-19 00:16:37 +0300

    SimpleCalc

---
+++ src/testapp1/SimpleCalc.java
@@ -0,0 +1,17 @@
+
+package testapp1;
+
+public class SimpleCalc {
+
+private double a;
+private double b;
+private double c;
+SimpleCalc(double p1,double p2) {
+this.a=p1;
+this.b=p2;
+}
+public double getSum() {
+return this.a+this.b;
+}
+
+}
