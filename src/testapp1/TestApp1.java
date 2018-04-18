945cd7d53ade1d2bd57fa41ce42c4a72333afd42
Tanya Kozlova <1eo5old99@gmail.com>
2018-04-19 00:04:13 +0300

    Creating SimpleCalc

---
+++ src/testapp1/TestApp1.java
@@ -0,0 +1,16 @@
+
+package testapp1;
+
+public class TestApp1 {
+   public static void main(String[] args) {
+        // TODO code application logic here
+float a=10;
+float b=(float) 12.5;
+System.out.println("a+b="+(a+b));
+ 
+  
+System.out.println("GIT test application");
+        // TODO code application logic here
+System.out.println("GIT test application");
+}
+}
